6a7975bdd2491e98989fb8a4380f0d7d1fa29da8
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-21 13:51:03 +0300

    add Home page with welcome text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Welcome from "./components/Welcome"
+import { Home } from "./pages/Home"
 
 function App() {
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Welcome />
+      <Home />
     </>
   )
 }
